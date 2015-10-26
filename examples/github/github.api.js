329ab178109fb11b38d631be6d8929aba7558594
palanik <palani.kumanan@gmail.com>
2015-10-25 21:38:19 -0400

    github.com api - https://developer.github.com/v3/

---
+++ examples/github/github.api.js
@@ -0,0 +1,14 @@
+var endpoints = require('./github.api.json');
+var wrapi = require('../../index.js');
+
+var opts = {
+	headers: {
+		'User-Agent': 'wrapi-client'
+	}
+};
+
+var client = new wrapi('https://api.github.com/', 
+						endpoints, 
+						opts);
+
+module.exports = client;
